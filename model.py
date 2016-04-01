d8e5e284655776393cba1c9afb33099630be97b6
Yonatan Halpern <yhalpern@Yonis-MacBook-Pro.local>
2016-04-01 14:45:53 -0400

    small cleanup

--- model.py
+++ model.py
@@ -1,41 +1,27 @@
-from pymc3 import Bernoulli, Beta, Deterministic, Model, Metropolis, sample, find_MAP
+from pymc3 import *
 import numpy as np
-import cPickle as pickle
-import itertools
 import theano.tensor as T 
-from theano.compile.ops import as_op
-
-def flatten(l):
-    return list(itertools.chain.from_iterable(l))
-            
-# p(noisy-label = 1)
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    S = 10
-    N = 10
-    alpha_p = 1
-    beta_p = 1
-    alpha_e = 1
-    beta_e = 10
-    observed_labels = np.array(np.random.rand(S,N) > 0.5, dtype=int)
+  S = 3
+  N = 3
+  alpha_p = 1
+  beta_p = 1
+  alpha_e = 1
+  beta_e = 10
+  observed_labels = np.array(np.random.rand(S,N) > 0.5, dtype=int)
 
-    @as_op(itypes=[T.lvector, T.dvector, T.dvector], otypes=[T.dmatrix])
-    def p_pos(l, e_pos, e_neg):
-        return np.outer(1-l,e_neg)  + np.outer(l, 1-e_pos)
+  with Model() as model:
+    p = Beta('p', alpha=alpha_p, beta=beta_p) #prior on true label
+    l = Bernoulli('l', p=p, shape=S) #true label
+    e_pos = Beta('e_pos', alpha_e, beta_e, shape=N) # error rate if label = 1
+    e_neg = Beta('e_neg', alpha_e, beta_e, shape=N) # error rate if label = 0
+    r = Deterministic('r',T.outer(1-l,e_neg)  + T.outer(l, 1-e_pos))  # P(noisy_label = 1) = e_neg if l == 0; 1-e_pos if l == 1
+    noisy_label = Bernoulli('noisy_label', p = r, shape=(S,N), observed=observed_labels)
 
-    with Model() as model:
-            p = Beta('p', alpha=alpha_p, beta=beta_p) #prior on true label
-            l = Bernoulli('l', p=p, shape=S) #true label
-            e_pos = Beta('e_pos', alpha_e, beta_e, shape=N) # error rate if label = 1
-            e_neg = Beta('e_neg', alpha_e, beta_e, shape=N) # error rate if label = 0
-
-            #r = Deterministic('r', p_pos(l, e_pos, e_neg))
-            r = Deterministic('r',T.outer(1-l,e_neg)  + T.outer(l, 1-e_pos)) 
-            
-            #noisy label
-            noisy_label = Bernoulli('noisy_label', p = r, shape=(S,N), observed=observed_labels)
-            start = find_MAP()
-            step = Metropolis()
-            trace = sample(2000, step=step, start=start, progressbar=True)
-            traceplot(trace)
-
+    start = find_MAP()
+    step = Metropolis()
+    trace = sample(2000, step=step, start=start, progressbar=True)
+    traceplot(trace)
+    plt.show()
