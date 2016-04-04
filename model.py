1761d0f118e2e3956486a7e4344684ad5606d82b
Yonatan Halpern <yhalpern@Yonis-MacBook-Pro.local>
2016-04-04 11:54:57 -0400

    model and test updates

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from pymc import Bernoulli, Beta, deterministic, MCMC, Deterministic
+from pymc import Bernoulli, Beta, deterministic, MCMC, Deterministic, Container
 import numpy as np
 import cPickle as pickle
 import itertools
@@ -13,43 +13,38 @@ def p_pos(l, e_pos, e_neg):
     else:
         return e_neg   #make a mistake
 
-if __name__ == "__main__":
-    S = 10
-    N = 10
-    alpha_p = 1
-    beta_p = 1
-    alpha_e = 1
-    beta_e = 10
-    observed_f = np.array(np.random.rand(S,N) > 0.5, dtype=int)
 
-    p = Beta('p', alpha=alpha_p, beta=beta_p) #prior on true label
-    l = [Bernoulli('l_'+str(i), p=p) for i in xrange(S)] #true label
-    e_pos = [Beta('e_pos'+str(j), alpha_e, beta_e) for j in xrange(N)] # error rate if label = 1
-    e_neg = [Beta('e_neg'+str(j), alpha_e, beta_e) for j in xrange(N)] # error rate if label = 0
+def estimate_failures(samples, n_samples, burn=0, thin=10, alpha_p=1, beta_p=1, alpha_e=1, beta_e=10):
+  S,N = samples.shape
+  p = Beta('p', alpha=alpha_p, beta=beta_p) #prior on true label
+  l = [Bernoulli('l_'+str(i), p=p) for i in xrange(S)] #true label
+  e_pos = [Beta('e_pos'+str(j), alpha_e, beta_e) for j in xrange(N)] # error rate if label = 1
+  e_neg = [Beta('e_neg'+str(j), alpha_e, beta_e) for j in xrange(N)] # error rate if label = 0
 
-    rate = [[] for _ in xrange(S)]
-    for i in xrange(S):
-        for j in xrange(N):
-            r = Deterministic(eval = p_pos,
+  rate = np.empty((S,N), dtype=object)
+  for i in xrange(S):
+      for j in xrange(N):
+          rate[i,j] = Deterministic(eval = p_pos,
                                     name = 'p_pos_'+str(i)+'m'+str(j),
                                     parents = {'l': l[i],
                                                'e_pos': e_pos[j],
                                                'e_neg': e_neg[j]},
-                                    doc = 'The rate of errors.',
                                     trace = True,
+                                    doc='',
                                     verbose = 0,
                                     dtype=float,
                                     plot=False,
                                     cache_depth = 2)
 
-            rate[i].append(r)
-    
-    #noisy label
-    noisy_label = np.array([[Bernoulli('f_'+str(i)+','+str(j), p = rate[i][j],
-                             value=observed_f[i,j]) for j in xrange(N)] for i in xrange(S)],
-                          dtype=object) 
+  
+  #noisy label
+  noisy_label = np.array([[
+    Bernoulli('f_'+str(i)+','+str(j), p = rate[i,j], value=samples[i,j]) for j in xrange(N)] for i in xrange(S)],
+      dtype=object) 
 
-    variables = set(l) | set([p]) | set(e_pos) | set(e_neg) | set(flatten(rate))
-    model = MCMC(variables)
-    model.sample(iter=100, burn=10, thin=2)
-    model.write_csv("out.csv", variables=["e_pos"+str(j) for j in xrange(N)]+["e_neg"+str(j) for j in xrange(N)])
+  variables = l + e_pos +e_neg + [p, Container(rate), Container(noisy_label)]
+  model = MCMC(variables)
+  model.sample(iter=n_samples, burn=burn, thin=thin)
+  
+  model.write_csv("out.csv", variables=['p']+["e_pos"+str(j) for j in xrange(N)]+["e_neg"+str(j) for j in xrange(N)])
+  return model
